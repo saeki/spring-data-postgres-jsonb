d50cc2aae8baf6cc81ecfd345cdbdb980b457e28
Koichi Saeki <saeki@users.noreply.github.com>
2020-06-21 10:32:30 +0900

    :arrow_up: Upgrade to hibernate-types-52 2.9.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.vladmihalcea:hibernate-types-52:2.9.5")
+    implementation("com.vladmihalcea:hibernate-types-52:2.9.12")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
